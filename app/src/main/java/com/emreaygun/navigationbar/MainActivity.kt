c8adbd4d39402495b00574c4adc3d34ef644f139
emreaygun10 <emreaygun1157@hotmail.com>
2021-08-28 18:38:48 +0300

    Navigate
    bug

--- app/src/main/java/com/emreaygun/navigationbar/MainActivity.kt
+++ app/src/main/java/com/emreaygun/navigationbar/MainActivity.kt
@@ -2,12 +2,10 @@ package com.emreaygun.navigationbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import com.emreaygun.navigationbar.fragments.FavoriteFragment
 import com.emreaygun.navigationbar.fragments.HomeFragment
 import com.emreaygun.navigationbar.fragments.SettingFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,23 +20,21 @@ class MainActivity : AppCompatActivity() {
         val fragmentManager =supportFragmentManager
         val fragmentTransaction=fragmentManager.beginTransaction()
 
-        val bottomNavigationView by lazy {
-            findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        }
 
-
-         bottomNavigationView.setOnNavigationItemReselectedListener {
+         bottom_navigation.setOnNavigationItemReselectedListener {
             when(it.itemId){
-                R.id.ic_home->fragmentTransaction.replace(R.id.fl_wrapper,favoriteFragment).commit()
-                R.id.ic_favorite->fragmentTransaction.replace(R.id.fl_wrapper,favoriteFragment).commit()
-                R.id.ic_setting->fragmentTransaction.replace(R.id.fl_wrapper,settingFragment).commit()
+                R.id.ic_home->fragmentTransaction.add(R.id.fl_wrapper,homeFragment).commit()
+                R.id.ic_favorite->fragmentTransaction.add(R.id.fl_wrapper,favoriteFragment).commit()
+                R.id.ic_setting->fragmentTransaction.add(R.id.fl_wrapper,settingFragment).commit()
             }
         }
+
 
     }
     private fun makeCurrentFragment(fragment: Fragment)=supportFragmentManager.beginTransaction().apply {
         replace(R.id.fl_wrapper,fragment)
         commit()
     }
+
 
     }
--- app/src/main/java/com/emreaygun/navigationbar/fragments/HomeFragment.kt
+++ app/src/main/java/com/emreaygun/navigationbar/fragments/HomeFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.emreaygun.navigationbar.R
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class HomeFragment : Fragment() {
-
+    val fragmentManager =susupportFragmentManager
+    val fragmentTransaction=fragmentManager.beginTransaction()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         }
 
@@ -27,8 +30,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var name=HomeFragmentDirections.actionHomeFragmentToSettingFragment("nice")
-        println(name.toString())
+        var name=HomeFragmentDirections.actionHomeFragmentToSettingFragment("emre")
+         Navigation.findNavController(view).navigate(name)
+        bottom_navigation.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.ic_home->fragmentTransaction.add(R.id.fl_wrapper,HomeFragment()).commit()
+                R.id.ic_favorite->fragmentTransaction.add(R.id.fl_wrapper,FavoriteFragment()).commit()
+                R.id.ic_setting->fragmentTransaction.add(R.id.fl_wrapper,SettingFragment()).commit()
+            }
+        }
     }
+
 
 }
